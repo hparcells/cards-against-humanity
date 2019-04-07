84678a47d9706b8a020f3c3e9e9f4baca167bf6f
Hunter Parcells <hparcells@icloud.com>
2019-04-07 10:16:56 -0400

    Connected Players in Pre-Game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,6 +3,7 @@ import { hot } from 'react-hot-loader/root';
 import { withStyles } from '@material-ui/core/styles';
 import Beforeunload from 'react-beforeunload';
 import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
 
 import TheActualGame from './TheActualGame';
 
@@ -22,11 +23,28 @@ class Game extends Component {
         <Beforeunload onBeforeunload={disconnect} />
         {
           !game.started
-            ? game.players.length >= 4
-              ? username === game.players[0].username
-                ? <Button variant='outlined' color='primary' className={classes.button} onClick={start}>Start with {game.players.length} Players</Button>
-                : <Button variant='outlined' color='primary' className={classes.button} disabled onClick={start}>Start with {game.players.length} Players (Only the Host Can Start the Game)</Button>
-              : <Button variant='outlined' color='primary' disabled className={classes.button}>Start ({game.players.length} of 4 Players)</Button>
+            ? <>
+                {
+                  game.players.length >= 4
+                    ? username === game.players[0].username
+                      ? <Button variant='outlined' color='primary' className={classes.button} onClick={start}>Start with {game.players.length} Players</Button>
+                      : <Button variant='outlined' color='primary' className={classes.button} disabled onClick={start}>Start with {game.players.length} Players (Only the Host Can Start the Game)</Button>
+                    : <Button variant='outlined' color='primary' disabled className={classes.button}>Start ({game.players.length} of 4 Players)</Button>
+                }
+
+                <Typography variant='h4' style={{ marginTop: '20px' }}>Connected Players</Typography>
+                <ul>
+                  {
+                    game.players.map((player) => {
+                      return <li><Typography>{
+                        player.username === username
+                          ? <strong>{player.username} (You)</strong>
+                          : player.username
+                      }</Typography></li>;
+                    })
+                  }
+                </ul>
+              </>
             : <TheActualGame username={username} game={game} playCard={playCard} czarPick={czarPick} />
         }
       </div>
